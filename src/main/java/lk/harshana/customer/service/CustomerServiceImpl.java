0fad5a5b994bf361b14e98dfad70a62318be944d
jmhmjayamaha <jmhmjayamaha@gmail.com>
2017-05-04 22:05:26 +0530

    model, repository , servce layers for customer

---
+++ src/main/java/lk/harshana/customer/service/CustomerServiceImpl.java
@@ -0,0 +1,59 @@
+package lk.harshana.customer.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import lk.harshana.customer.repository.CustomerRepository;
+import lk.harshana.model.Customer;
+
+@Service("customerService")
+public class CustomerServiceImpl implements CustomerService {
+
+	@Autowired
+	private CustomerRepository customerRepository;
+	
+	@Override
+	public void createCustomer(Customer customer) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Customer> findAllCustomers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Customer findCustomerByName(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Customer findCustomerByAddress(String address) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Customer findCustomerByTelNo(String telNo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updateCustomer(int id, Customer customer) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean deleteCustomer(int id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
